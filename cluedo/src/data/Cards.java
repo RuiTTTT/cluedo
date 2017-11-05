6fa87d42ce3b1b2d5aed65c9a7e6a2a52b3a0c4d
seraphimTT <767731843@qq.com>
2017-11-05 12:20:29 +0000

    data package

--- cluedo/src/data/Cards.java
+++ cluedo/src/data/Cards.java
@@ -2,9 +2,9 @@ package data;
 
 public abstract class Cards {
 	public enum cardType {SUSPECTS, WEAPONS, ROOMS}
-	public final String[] WEAPONS = {Weapon.CANDLESTICK, Weapon.KNIFE, Weapon.LEADPIPE, Weapon.POISON, Weapon.REVOLVER, Weapon.ROPE};
-	public final String[] SUSPECTS = {Suspects.GREEN, Suspects.Mustard, Suspects.PEACOCK, Suspects.PLUM, Suspects.SCARLETT, Suspects.WHITE};
-	public final String[] ROOMS = {Room.BALLROOM, Room.BILLIARD, Room.CONSERVATORY, Room.DININGROOM, Room.HALL, Room.KITCHEN, Room.LIBRARY, Room.LOUNGE, Room.STUDY};
+	public static final  String[] WEAPONS = {Weapon.CANDLESTICK, Weapon.KNIFE, Weapon.LEADPIPE, Weapon.POISON, Weapon.REVOLVER, Weapon.ROPE};
+	public static final String[] SUSPECTS = {Suspects.GREEN, Suspects.Mustard, Suspects.PEACOCK, Suspects.PLUM, Suspects.SCARLETT, Suspects.WHITE};
+	public static final String[] ROOMS = {Room.BALLROOM, Room.BILLIARD, Room.CONSERVATORY, Room.DININGROOM, Room.HALL, Room.KITCHEN, Room.LIBRARY, Room.LOUNGE, Room.STUDY};
 	
 	
 }
